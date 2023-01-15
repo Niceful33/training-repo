46f69663cbfb5e58f406061b7e61417399a66b14
Nadia Kalynjuk <103382849+Niceful33@users.noreply.github.com>
2023-01-15 13:55:01 +0100

    add2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,12 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryMarkup = galleryItems.reduce(
+  (acc, item) =>
+    acc +
+    `<a class="gallery__link" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`,
+  ''
+);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
